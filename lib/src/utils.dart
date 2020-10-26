f0913f67542974fef42067ec21ce755caba04717
DUC PHAN <53320799+DucPhan0306@users.noreply.github.com>
2020-10-27 00:55:04 +0700

    Update utils.dart

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -12,8 +12,8 @@ const defaultLegendStyle = TextStyle(
 );
 
 const List<Color> defaultColorList = [
-  Color(0xFFff7675),
   Color(0xFF74b9ff),
+  Color(0xFFff7675),
   Color(0xFF55efc4),
   Color(0xFFffeaa7),
   Color(0xFFa29bfe),
